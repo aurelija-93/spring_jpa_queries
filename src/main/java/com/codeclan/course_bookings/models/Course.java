8e69242de4f98e2bc88bce5e105d94e15ba90f7e
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-31 11:23:49 +0100

    added Customer class

--- src/main/java/com/codeclan/course_bookings/models/Course.java
+++ src/main/java/com/codeclan/course_bookings/models/Course.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.ArrayList;
+
 @Entity
 public class Course {
     @Id
@@ -13,4 +15,51 @@ public class Course {
     private String name;
     private String location;
     private Rating rating;
+    private ArrayList<Booking> bookings;
+
+    public Course(String name, String location, Rating rating) {
+        this.name = name;
+        this.location = location;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
--- src/main/java/com/codeclan/course_bookings/models/Customer.java
+++ src/main/java/com/codeclan/course_bookings/models/Customer.java
@@ -1,4 +1,65 @@
 package com.codeclan.course_bookings.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private int age;
+    private ArrayList<Booking> bookings;
+
+    public Customer(String name, String location, int age) {
+        this.name = name;
+        this.location = location;
+        this.age = age;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Customer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBookings(ArrayList<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
