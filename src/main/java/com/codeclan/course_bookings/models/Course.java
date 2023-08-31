b226ce4b7fe69f36d83519662a9b4f5ca52c2bac
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-31 11:19:34 +0100

    added Course class & Rating enum

--- src/main/java/com/codeclan/course_bookings/models/Course.java
+++ src/main/java/com/codeclan/course_bookings/models/Course.java
@@ -1,4 +1,16 @@
 package com.codeclan.course_bookings.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private Rating rating;
 }
