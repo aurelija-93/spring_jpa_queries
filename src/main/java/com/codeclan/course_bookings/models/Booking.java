beffb3d4ca77ec7fca2a6550e821d3ee273cd061
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-31 11:25:46 +0100

    added Booking class

--- src/main/java/com/codeclan/course_bookings/models/Booking.java
+++ src/main/java/com/codeclan/course_bookings/models/Booking.java
@@ -1,4 +1,53 @@
 package com.codeclan.course_bookings.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String date;
+    private Course course;
+    private Customer customer;
+
+    public Booking(String date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Booking() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
